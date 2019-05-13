ecdd8012f180ad5dda06164a80c313572aae3516
Jessica Sánchez <alumno@sldid10-3.local>
2019-05-13 09:10:00 -0500

    third

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -8,40 +8,99 @@ export default class ApplicationController extends Controller {
 		let r1,r2,r3;
 		if (numero1) {
 			console.log(true);
-			this.setState({r1:"lol"});
-		}else
+			 this.State = {
+      		...this.State,
+     		 r1:"true"
+    		}
+		}else{
 		console.log(false);
+		this.State = {
+      		...this.State,
+     		 r1:"false"
+    		}
+		}	
 		if (numero1) {
 			console.log(true);
-		}else
+			this.State = {
+      		...this.State,
+     		 r2:"true"
+    		}
+		}else{
 		console.log(false);
+		this.State = {
+      		...this.State,
+     		 r2:"false"
+    		}
+	}
 		if (numero1) {
 			console.log(true);
-		}else
+			this.State = {
+      		...this.State,
+     		 r3:"true"
+    		}
+		}else{
 		console.log(false);
+		this.State = {
+      		...this.State,
+     		 r3:"false"
+    		}
+	}	
 		return false;
 	}
 	@action
 	odd(numero1, numero2, numero3){
 		if (numero1%2===0) {
+			this.State = {
+      		...this.State,
+     		 r1:"false"
+    		}
 			console.log(false);
-		}else
+		}else{
 		console.log(true);
+		this.State = {
+      		...this.State,
+     		 r1:"true"
+    		}
+	}
 		if (numero2%2===0) {
+			this.State = {
+      		...this.State,
+     		 r2:"false"
+    		}
 			console.log(false);
-		}else
+		}else{
 		console.log(true);
+		this.State = {
+      		...this.State,
+     		 r2:"true"
+    		}
+	}
 		if (numero3%2===0) {
 			console.log(false);
-		}else
+			this.State = {
+      		...this.State,
+     		 r3:"false"
+    		}
+		}else{
+			this.State = {
+      		...this.State,
+     		 r3:"true"
+    		}
 		console.log(true);
 		return true;
+	}
 	}
 	@action
 	pow(numero1, numero2, numero3){
 		console.log(numero1*numero1);
 		console.log(numero2*numero2);
 		console.log(numero3*numero3);
+		this.State = {
+      		...this.State,
+     		 r1:numero1*numero1,
+     		 r2:numero2*numero2,
+     		 r3:numero3*numero3
+    		}
 		return numero1*numero1;
 	}
 	@tracked State={
